0750adb22eb84756763b47a9946d307a9637af77
Emerson Viana <missonzito@outlook.com>
2026-07-25 16:40:40 -0300

    Estrutura inicial do site Denis de Melo

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Historia from "./components/Historia";
+import Causas from "./components/Causas";
+import Comunidade from "./components/Comunidade";
+import Participacao from "./components/Participacao";
+import Contato from "./components/Contato";
+import Footer from "./components/Footer";
+
+
+function App() {
+
+return(
+
+<>
+
+<Navbar />
+
+<Hero />
+
+<Historia />
+
+<Causas />
+
+<Comunidade />
+
+<Participacao />
+
+<Contato />
+
+<Footer />
+
+</>
+
+)
+
+}
+
+
+export default App;
